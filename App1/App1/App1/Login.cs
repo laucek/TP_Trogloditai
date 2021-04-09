8bf254704508eda8401e520f1b4669a9006d12f6
Pauli <Pauli@DESKTOP-I651VFF>
2021-04-09 20:14:07 +0300

    loginas2

--- App1/App1/App1/Login.cs
+++ App1/App1/App1/Login.cs
@@ -11,11 +11,15 @@ namespace App1
 	{
 		public Login ()
 		{
-            Content = new StackLayout {
-                Children = {
-                    new Label { Text = "Loginas",
-                        BackgroundColor = Color.Red
-                    }
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "- Login into your account -", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.StartAndExpand },
+					new Entry { Placeholder = "Enter your account name: ", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    new Entry { Placeholder = "Enter your password: ", IsPassword = true, HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+					
+
+					new Label { Text = "Loginas", BackgroundColor = Color.Red }
+
 				}
 			};
 		}
