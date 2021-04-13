75dc8e0df7bfd7f4cb9dae88490397020ac89756
Laucek <43381869+Unholy57@users.noreply.github.com>
2021-04-13 18:26:58 +0300

    Pridedu User type'a ir SQLite'o connectiona

---
+++ App1/App1/App1/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string first_name { get; set; }
+        public DateTime registration_date { get; set; }
+
+        public User(int id, string username, string email, string password, string first_name, DateTime registration_date)
+        {
+            this.id = id;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.first_name = first_name;
+            this.registration_date = registration_date;
+        }
+
+        public override string ToString()
+        {
+            return $"{first_name} {username} {email} {password} {registration_date}";
+        }
+    }
+}
