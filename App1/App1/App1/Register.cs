1829b31805d2e3c50b34a2a75a3c9303e26ee310
Laucek <43381869+Unholy57@users.noreply.github.com>
2021-04-13 18:45:03 +0300

    Cookie cutter Register

--- App1/App1/App1/Register.cs
+++ App1/App1/App1/Register.cs
@@ -2,20 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SQLite;
 
 using Xamarin.Forms;
 
 namespace App1
 {
-	public class Register : ContentPage
-	{
-		public Register ()
-		{
-			Content = new StackLayout {
-				Children = {
-					new Label { Text = "Welcome to Xamarin.Forms!" }
-				}
-			};
-		}
-	}
+    public class Register : ContentPage
+    {
+        Entry firstNameEntry;
+        public Register()
+        {
+
+            Button butt = new Button
+            {
+                Text = "Register",
+                BackgroundColor = Color.White,
+                BorderColor = Color.Black,
+                BorderWidth = 3,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            firstNameEntry = new Entry
+            {
+                Text = "Enter your first name",
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            
+            butt.Clicked += async (sender, args) => OnButtonClicked(sender, args, butt);
+
+            Content = new StackLayout
+            {
+                Children = {
+                    //First name
+					new Label { Text = "First name:*", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    firstNameEntry,
+
+                    //Email
+                    new Label { Text = "E-Mail address:*", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    new Entry { Text = "Enter your e-mail here", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+
+
+                    //Password
+                    new Label { Text = "Password: *", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    new Entry { Text = "Enter your password", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+
+                    //Password confirmation
+                    new Label { Text = "Password confirmation*", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    new Entry { Text = "Confirm password", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+
+                    butt
+
+                    //new Button { Text = "Register", BackgroundColor = Color.White, BorderColor = Color.Black, BorderWidth = 3,
+                    //HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand }
+
+                    //new Label { Text = "First name*", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
+                    //new Entry { Text = "Enter your first name", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand }
+                }
+
+            };
+        }
+
+        void OnButtonClicked(object sender, EventArgs args, Button butt)
+        {
+            butt.Text = firstNameEntry.Text;
+        }
+    }
 }
