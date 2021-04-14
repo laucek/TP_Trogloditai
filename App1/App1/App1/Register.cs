705f49f8c2901f872ce2fae48b7a01505e632acc
Laucek <43381869+Unholy57@users.noreply.github.com>
2021-04-14 21:38:17 +0300

    Turetu veikt jau

--- App1/App1/App1/Register.cs
+++ App1/App1/App1/Register.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.SQLite;
 
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace App1
 {
@@ -17,6 +18,8 @@ namespace App1
         Entry emailEntry;
         Entry password;
         Entry passwordconfirm;
+
+        Label errorLabel;
 
         public Register()
         {
@@ -34,14 +37,14 @@ namespace App1
 
             firstNameEntry = new Entry
             {
-                Placeholder = "Enter your first name",
+                Placeholder = "Atleast 3 characters",
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
             username = new Entry
             {
-                Placeholder = "Enter your user name",
+                Placeholder = "Atleast 3 characters",
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
@@ -55,19 +58,28 @@ namespace App1
 
             password = new Entry
             {
-                Placeholder = "Enter your password",
+                Placeholder = "Atleast 3 characters",
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
             passwordconfirm = new Entry
             {
-                Placeholder = "Confirm your passsword",
+                Placeholder = "Atleast 3 characters",
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
-            butt.Clicked += async (sender, args) => OnButtonClicked(sender, args, butt);
+            errorLabel = new Label
+            {
+                IsEnabled = false,
+                Text = "Something went wrong, make sure your inputs meet the criteria",
+                TextColor = Color.Red,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            butt.Clicked += async (sender, args) => await OnButtonClicked(sender, args, butt);
 
             ScrollView scrollView = new ScrollView
             {
@@ -97,38 +109,41 @@ namespace App1
                         new Label { Text = "Password confirmation*", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand },
                         passwordconfirm,
 
-                        butt
+                        butt,
+                        
+                        
                     }
                 }
             };
 
             Content = scrollView;
-            //{
-                
-            //    Children = {
-                    
-
-            //        //new Button { Text = "Register", BackgroundColor = Color.White, BorderColor = Color.Black, BorderWidth = 3,
-            //        //HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand }
-
-                    
-            //    }
-
-            //};
         }
 
-        void OnButtonClicked(object sender, EventArgs args, Button butt)
+        async Task OnButtonClicked(object sender, EventArgs args, Button butt)
         {
+            if (!MeetsCriteria())
+            {
+                errorLabel.IsEnabled = true;
+                return;
+            }
 
             User user = new User(0, username.Text, emailEntry.Text, password.Text, firstNameEntry.Text, DateTime.Now);
 
             butt.Text = MySQLManager.InsertUser(user);
 
+            await Navigation.PushAsync(new Login());
         }
 
         bool MeetsCriteria()
         {
-            return true;
+            if(firstNameEntry.Text.Length >= 3 && username.Text.Length >= 3 && emailEntry.Text.Length >= 6 && emailEntry.Text.Contains("@") && password.Text.Length >= 3)
+            {
+                if(password.Text == passwordconfirm.Text)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
